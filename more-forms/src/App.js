8299a080a6d4407a9d8eca5ed7f95f8c0801f716
geovonnie vasquez <geovonnievasquez@gmail.com>
2020-12-18 18:19:29 -0600

    state added

--- more-forms/src/App.js
+++ more-forms/src/App.js
@@ -3,10 +3,8 @@ import UserForm from "./components/UserForm"
 import Results from "./components/Results"
 
 function App() {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+
+  
 
   return (
     <div className="App">
--- more-forms/src/components/UserForm.jsx
+++ more-forms/src/components/UserForm.jsx
@@ -1,10 +1,33 @@
-import react, { useState } from 'react';
+import React from 'react';
 
 const UserForm = () => {
-    return (
-       <form>
-           
-       </form>
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    return (        
+       <div>
+           <h1>User Form</h1>
+           <form>
+               <div>
+                <label>First Name: </label>
+                <input type="text" name="firstName"/>
+               </div>
+               <div>
+                <label>Last Name: </label>
+                <input type="text" name="lastName"/>
+               </div>
+               <div>
+                <label>Email: </label>
+                <input type="text" name="email"/>
+               </div>
+               <div>
+                <label>Password: </label>
+                <input type="text" name="password"/>
+               </div>
+           </form>
+       </div>
     );
 };
     
